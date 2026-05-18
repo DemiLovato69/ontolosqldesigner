3ed7c70ee81bb94c7ced937a14d8ca6800272c2b
snydi <d-snyatkov@mail.ru>
2026-05-18 23:30:15 +0300

    fix: mail problem

--- backend/app/Jobs/SendVerificationEmail.php
+++ backend/app/Jobs/SendVerificationEmail.php
@@ -15,9 +15,9 @@ class SendVerificationEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public int $tries = 3;
+    public int $tries = 5;
     public int $timeout = 30;
-    public int $backoff = 60;
+    public int $backoff = 5;
     public bool $deleteWhenMissingModels = true;
 
     public function __construct(private readonly User $user)
