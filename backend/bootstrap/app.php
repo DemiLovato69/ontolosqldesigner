e760d59efb1cbac59d64473e75984b88f8cd596c
Demetry <demetryromanowski@gmail.com>
2026-06-10 11:03:58 -0400

    Updated proxy trust.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $isDiagramImport = fn ($request): bool => $request->is('api/diagrams/sql/import/*');
         $middleware->trimStrings(except: [$isDiagramImport]);
         $middleware->convertEmptyStringsToNull(except: [$isDiagramImport]);
